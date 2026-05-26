194872b0d0d76a2cb1b88bd6d709194a92411594
bodvarsdottirkristin <bodvarsdottirkristin@gmail.com>
2026-05-26 16:54:03 +0200

    fixing the config file

---
+++ .history/bucket_access/config_20260525113140.py
@@ -0,0 +1,7 @@
+# Configuration file for the Explainable Brains project.
+
+# Replace the X with your actual Hetzner Object Storage credentials and bucket information.
+HETZNER_ACCESS_KEY  = "X"
+HETZNER_SECRET_KEY  = "X"
+HETZNER_BUCKET_NAME = "explainable-brains"
+HETZNER_ENDPOINT    = "https://fsn1.your-objectstorage.com"
